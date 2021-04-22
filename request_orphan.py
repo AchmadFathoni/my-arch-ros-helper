65a3b95485259d213ef1ecb99ec71044aff56b14
Achmad Fathoni <fathoni.id@gmail.com>
2021-04-23 00:40:37 +0700

    Add orphan request and flagger

--- request_orphan.py
+++ request_orphan.py
@@ -12,20 +12,24 @@ r = s.post('https://aur.archlinux.org/login', data=login)
 
 f = open("outdated_aur.txt","r")
 PREFIX = 'https://aur.archlinux.org/pkgbase/'
+COMMENTS = """
+It's been more than 2 months since the maintainer is too occupied to maintain any ros-noetic-* packages and those packages are out-of-date[3]
+
+[3]https://github.com/ros-noetic-arch/ros-noetic-desktop-full/issues/20
+"""
 for line in f.readlines():
     package = line.split()[1][:-1]
-    new_version = line.split()[3].strip("()").split('-')[0]
-    url = PREFIX+package+'/flag/'
-    p = s.get(url)
+    p = s.get(PREFIX+package+'/request/')
 
     payload = {}
     tree = html.fromstring(p.content)
     payload['ID'] = tree.xpath('//input[@name="ID"]/@value')[0]
     payload['IDs['+payload['ID']+']'] = '1'
     payload['token'] = tree.xpath('//input[@name="token"]/@value')[0]
-    payload['comments'] = new_version + ' released'
-    payload['do_Flag'] = 'Flag'
-
-    p = s.post(url, data=payload)
+    payload['comments'] = COMMENTS
+    payload['type'] = 'orphan'
+    payload['merge_into'] = ''
+    payload['do_FileRequest'] = "Submit Request"
+    p = s.post(PREFIX, data=payload)
     print(package, " ", p.status_code)
     time.sleep(5)
